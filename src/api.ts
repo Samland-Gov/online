58f73eb91138d460d600f37cf6a7a1812eb177f8
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2024-11-19 10:55:45 +0000

    feat: view by expression + table of contents

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import {type Work, type Expression} from './models'
 
 const api_url = 'https://legislation.minersonline.uk/api/v3/akn/zl/.json';
+const default_language_code = "en";
 
 export async function request_with_token(url: string) {
     const response = await fetch(url, {
@@ -22,16 +23,15 @@ export async function pull_works(): Promise<Work[]> {
     let url = api_url;
     while (url != null) {
         let resp = await (await request_with_token(url)).json();
-        for (const index in resp["results"]) {
-            const data = resp["results"][index];
+        for (const result of resp["results"]) {
             const expressions = [] as Expression[];
             const work: Work = {
-                frbrUri: data['frbr_uri'],
-                title: data['title'],
-                metadata: data,
+                frbrUri: result['frbr_uri'],
+                title: result['title'],
+                metadata: result,
                 expressions: expressions
             };
-            for (const date of data["points_in_time"]) {
+            for (const date of result["points_in_time"]) {
                 for (const expression of date["expressions"]) {
                     await create_expression(expression, work);
                 }
@@ -56,4 +56,17 @@ async function create_expression(data: any, work: Work) {
         tocJson: await (await request_with_token(`${data["url"]}/toc.json`)).json()
     };
     work.expressions.push(expression);
+}
+
+export function default_expression(work: Work): Expression | undefined {
+    const expression = work.expressions.filter((work) => {
+        if (work.languageCode == default_language_code) {
+            return true;
+        }
+        return false;
+    })[0]
+    if (expression == undefined) {
+        return work.expressions[0]
+    }
+    return undefined
 }
